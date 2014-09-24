69d64c014f56a6f494877b245d45bcfdb9ca5380
Julien Gilli <julien.gilli@joyent.com>
2014-09-24 11:51:55 -0700

    Determine core files path dynamically depending on the platform.

--- exercises/call_stack/exercise.js
+++ exercises/call_stack/exercise.js
@@ -2,6 +2,8 @@
 
 var exercise = require('workshopper-exercise')();
 var path     = require('path');
+var util     = require('util');
+var debug    = require('debug')('debug-school');
 var mv       = require('mv');
 
 var config = require('../../config.js');
@@ -9,6 +11,8 @@ var dumpCore = require('../../lib/dumpcore.js');
 
 exercise.addPrepare(function(callback) {
     dumpCore(path.join(__dirname, 'node-script-that-aborts.js'), function(err, coreFilePath) {
+      debug(util.format('core dumped at path [%s]', coreFilePath));
+
       if (!err) {
         var coreFileName = path.basename(coreFilePath);
         var dstCoreFilePath = path.join(config.CORE_FILES_DIRECTORY,
