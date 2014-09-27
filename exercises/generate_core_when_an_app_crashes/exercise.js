b47d0bd8462c745e73ef84f02b1a33e44b55d5d6
Julien Gilli <julien.gilli@joyent.com>
2014-09-27 00:04:36 +0000

    Restore coreadm system config when cleaning up.

--- exercises/generate_core_when_an_app_crashes/exercise.js
+++ exercises/generate_core_when_an_app_crashes/exercise.js
@@ -4,16 +4,41 @@ var util = require('util');
 
 var exercise = require('workshopper-exercise')();
 var debug    = require('debug')('debug-school');
+var xtend    = require('xtend');
 
 var executeshellscript = require('../../lib/workshopper-exercise/executeshellscript');
 var isCoreFile         = require('../../lib/core/iscorefile.js');
+var coreConfig         = require('../../lib/core/coreconfig.js');
 
-var preCommands = ['ulimit -Sc 0 >/dev/null 2>&1'];
-if (process.platform === 'sunos') {
-    preCommands.push('coreadm -d process >/dev/null 2>&1');
-    preCommands.push('coreadm -d global >/dev/null 2>&1');
-    preCommands.push('coreadm -g /foo/bar >/dev/null 2>&1');
-}
+exercise.addSetup(function setup(mode, callback) {
+  if (mode === 'verify') {
+    coreConfig.getCoreFilesConfig(function(err, originalCoreFilesConfig) {
+      exercise.originalCoreFilesConfig = originalCoreFilesConfig;
+      var coreFilesConfig = xtend(originalCoreFilesConfig, {});
+      if (process.platform === 'sunos') {
+        debug('Disabling core dumps generation...');
+
+        coreFilesConfig.globalCoreDumps = false;
+        coreFilesConfig.perProcessCoreDumps = false;
+        coreFilesConfig.globalPattern = '/foo/bar';
+
+        coreConfig.applyConfig(coreFilesConfig, function coreConfigApplied(err) {
+          if (!err) {
+            debug('Core dumps generation disabled successfuly!');
+          } else {
+            debug('Could not disable core dumps generation, reason: ' + err);
+          }
+        
+          return callback(err);
+         });
+      }
+    });
+  }
+});
+
+exercise.addCleanup(function cleanup(mode, pass, callback) {
+  coreConfig.applyConfig(exercise.originalCoreFilesConfig);
+});
 
 function checkSolution(err, stdout, stderr, callback) {
   debug('checking solution...');
@@ -24,6 +49,7 @@ function checkSolution(err, stdout, stderr, callback) {
   return isCoreFile(util.format('/cores/core.%d', nodeAppPid), callback);
 }
 
+var preCommands = ['ulimit -Sc 0 >/dev/null 2>&1'];
 exercise = executeshellscript(exercise, preCommands, checkSolution);
 exercise.submissionName = 'shell-script.sh';
 
